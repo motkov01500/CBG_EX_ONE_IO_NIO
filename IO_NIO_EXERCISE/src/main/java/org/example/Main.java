d9768edfc1ce1d5b34e8b5524b42c8d97c0720ee
motkov01500 <liuboslavmotkov@gmail.com>
2024-02-28 17:47:09 +0200

    add select option encrypt or decrypt

--- IO_NIO_EXERCISE/src/main/java/org/example/Main.java
+++ IO_NIO_EXERCISE/src/main/java/org/example/Main.java
@@ -2,28 +2,35 @@ package org.example;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.Key;
 import java.util.Scanner;
 
-import static org.example.ZipOperationsService.*;
+import static org.example.ZipOperationsService.decryptZipFile;
+import static org.example.ZipOperationsService.encryptFile;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        Path toEncrypt;
-        Path newZip;
-        Cipher algorithm = Cipher.getInstance("AES");
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(128);
-        Key key = keyGenerator.generateKey();
-        System.out.print("Enter path for file to Encrypt:");
-        toEncrypt = Paths.get(scanner.next());
-        System.out.print("Enter path for file to Decrypt:");
-        newZip = Paths.get(scanner.next());
-//        encryptFile(algorithm,key,toEncrypt.toFile(),newZip.toFile());
-        decryptZipFile(algorithm,toEncrypt.toFile(),newZip.toFile());
+        try(Scanner scanner = new Scanner(System.in)) {
+            Path toEncrypt;
+            Path newZip;
+            Cipher algorithm = Cipher.getInstance("AES");
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128);
+            Key key = keyGenerator.generateKey();
+            System.out.print("Enter the first path:");
+            toEncrypt = Paths.get(scanner.next());
+            System.out.print("Enter the second path:");
+            newZip = Paths.get(scanner.next());
+            System.out.println("Enter the operation(encrypt or decrypt):");
+            String option = scanner.next();
+            switch (option.toLowerCase()) {
+                case "encrypt":
+                    encryptFile(algorithm, key, toEncrypt.toFile(), newZip.toFile());break;
+                case "decrypt":
+                    decryptZipFile(algorithm, toEncrypt.toFile(), newZip.toFile());break;
+            }
+        }
     }
 }
